3aa7e2a4eeda3c93f8a1f666cfc90cebcb0dc1cc
jotamaster <j3anhernandez@gmail.com>
2019-11-12 09:53:16 -0400

    add endpoit register user

--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -1,6 +1,7 @@
 const bcrypt = require('bcrypt');
 const { sign } = require('jsonwebtoken');
 const User = require('../models/User');
+var emailSecure = require('email-secure');
 
 exports.login = async (req, res) => {
 	try {
@@ -27,4 +28,32 @@ exports.login = async (req, res) => {
 	}
 };
 
-exports.register = async (req, res) => {};
+exports.register = async (req, res) => {
+	try {
+		let username = req.body.username;
+		let email = req.body.email;
+		let password = req.body.password;
+
+		if (!username && username >= 6) return res.status(409).json({ message: 'Conflict' });
+
+		if (!emailSecure.validateEmail(email)) return res.status(409).json({ message: 'Conflict' });
+
+		if (!password && password >= 6) return res.status(409).json({ message: 'Conflict paswword invalid' });
+
+		const user = await User.query().select('username').where('username', username);
+
+		if (user.length) return res.status(409).json({ message: 'el nombre de usuario ya existe' });
+
+		const emailExist = await User.query().select('email').where('username', email);
+
+		if (emailExist.length) return res.status(409).json({ message: 'el email ya existe' });
+
+		bcrypt.hash(password, 10, async function(err, hash) {
+			newUser = await User.query().insert({ username, email, password: hash });
+		});
+
+		return res.status(201).json({ message: 'usuario creado exitosamente' });
+	} catch (error) {
+		return res.status(500).json({ success: false, error: error.message });
+	}
+};
